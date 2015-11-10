95814c4d3e17c95789b871db4e7c84914d35d5a5
kaffeaffe <alfred.samuelson@icloud.com>
2015-11-10 14:00:42 +0900

    Add support for mBed Audio Codec (typo)

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -949,7 +949,7 @@ void __init bcm2708_init(void)
 	bcm_register_device_dt(&bcm2708_i2s_device);
 #endif
 
-#ifdef defined(CONFIG_SND_BCM2708_SOC_RPI_CODEC_MBED_MODULE) || defined(CONFIG_SND_BCM2708_SOC_RPI_CODEC_MBED)
+#ifdef defined(CONFIG_SND_BCM2708_SOC_RPI_CODEC_MBED) || defined(CONFIG_SND_BCM2708_SOC_RPI_CODEC_MBED_MODULE)
 	bcm_register_device(&snd_rpi_mbed_device);
 	i2c_register_board_info(1, snd_rpi_mbed_i2c_devices, ARRAY_SIZE(snd_rpi_mbed_i2c_devices));
 #endif
